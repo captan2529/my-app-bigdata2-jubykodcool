69a1a9e7819cc717adb7e34b88a0cac68fe36304
jubykodcool <jidaphx@gmail.com>
2024-07-19 15:07:18 +0700

    first commit

---
+++ src/app/landingpage/page.tsx
@@ -0,0 +1,10 @@
+import LandingPage from '@/components/LandingPage'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+        <LandingPage/>
+    </div>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Home() {
+  return <div>การวิเคราะห์ข้อมูลขนาดใหญ่ของรัฐประศาสนศาสตร์</div>;
+}
